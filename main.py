e0776507fbcaac9e1ee56f4e13ee9219f9031886
Dzen819 <43151543+Dzen819@users.noreply.github.com>
2021-08-25 17:56:03 +0300

    1

--- main.py
+++ main.py
@@ -4,8 +4,6 @@ from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
 
-
-
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
@@ -14,19 +12,24 @@ player = Player()
 score = Scoreboard()
 car = CarManager()
 
-
-
-
-
-
-
 screen.onkey(player.move, "Up")
 game_is_on = True
-
+num = 0
 while game_is_on:
     time.sleep(0.1)
     screen.update()
-    car.create_car()
+    if num == 0:
+        car.create_car()
+        num = 6
     car.move()
+    num -= 1
+    if player.ycor() >= 300:
+        score.level_up()
+        car.speed_up()
+        player.to_start()
+    for c in car.cars:
+        if c.distance(player) < 30:
+            score.game_over()
+            game_is_on = False
 
-    
+screen.exitonclick()
--- scoreboard.py
+++ scoreboard.py
@@ -1,5 +1,23 @@
+from turtle import Turtle
+
 FONT = ("Courier", 24, "normal")
 
 
-class Scoreboard:
-    pass
+class Scoreboard(Turtle):
+    def __init__(self):
+        self.level = 1
+        super().__init__()
+        self.hideturtle()
+        self.penup()
+        self.goto(-290, 260)
+        self.color("black")
+        self.write(f"Level: {self.level}", align="left", font=FONT)
+
+    def level_up(self):
+        self.level += 1
+        self.clear()
+        self.write(f"Level: {self.level}", align="left", font=FONT)
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("GAME OVER", align="center", font=FONT)
